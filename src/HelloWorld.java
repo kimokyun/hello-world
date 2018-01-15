a97593270f18e496ddf8a3a076ba524b06f29039
pinkyun <pinkyun@kimokyun>
2018-01-15 10:10:21 -0800

    hi world 메세지 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hi world");
-		System.out.println("hi java");
+		System.out.println("hi java_java");
 		
 		
 	}//배운것을 활용해서 연습하자,마우스키를 최대한으로 줄이자
